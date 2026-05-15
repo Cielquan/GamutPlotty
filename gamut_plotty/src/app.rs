76f030200f903ce749ad6009faaf2fb793c09770
Christian Riedel <cielquan@protonmail.com>
2026-05-15 13:15:07 +0200

    add dancing strings example as canvas dummy

--- gamut_plotty/src/app.rs
+++ gamut_plotty/src/app.rs
@@ -1,7 +1,7 @@
 use color_calc::CIELAB;
 use egui::{
-    Align, AtomExt, Button, CentralPanel, Image, IntoAtoms, Layout, MenuBar, Panel, Theme,
-    ThemePreference, include_image,
+    Align, AtomExt, Button, CentralPanel, Color32, Frame, Image, IntoAtoms, Layout, MenuBar, Panel,
+    Pos2, Rect, Theme, ThemePreference, emath, epaint, hex_color, include_image, pos2,
 };
 
 use crate::dummy_state::create_color_points;
@@ -147,7 +147,68 @@ impl eframe::App for GamutPlottyApp {
             // The central panel the region left after adding TopPanel's and SidePanel's
             ui.heading(APP_NAME);
 
-            ui.separator();
+            Frame::canvas(ui.style()).show(ui, |ui| {
+                ui.request_repaint();
+
+                let show_colors = true;
+                let color = if ui.visuals().dark_mode {
+                    Color32::from_additive_luminance(196)
+                } else {
+                    Color32::from_black_alpha(240)
+                };
+
+                let time = ui.input(|i| i.time);
+
+                let desired_size = ui.available_width() * egui::vec2(1.0, 0.35);
+                let (_id, rect) = ui.allocate_space(desired_size);
+
+                let to_screen = emath::RectTransform::from_to(
+                    Rect::from_x_y_ranges(0.0..=1.0, -1.0..=1.0),
+                    rect,
+                );
+
+                let mut shapes = vec![];
+
+                for &mode in &[2, 3, 5] {
+                    let mode = mode as f64;
+                    let n = 120;
+                    let speed = 1.5;
+
+                    let points: Vec<Pos2> = (0..=n)
+                        .map(|i| {
+                            let t = i as f64 / (n as f64);
+                            let amp = (time * speed * mode).sin() / mode;
+                            let y = amp * (t * std::f64::consts::TAU / 2.0 * mode).sin();
+                            to_screen * pos2(t as f32, y as f32)
+                        })
+                        .collect();
+
+                    let thickness = 10.0 / mode as f32;
+                    shapes.push(epaint::Shape::line(
+                        points,
+                        if show_colors {
+                            epaint::PathStroke::new_uv(thickness, move |rect, p| {
+                                let t = egui::remap(p.x, rect.x_range(), -1.0..=1.0).abs();
+                                let center_color = hex_color!("#5BCEFA");
+                                let outer_color = hex_color!("#F5A9B8");
+
+                                Color32::from_rgb(
+                                    egui::lerp(center_color.r() as f32..=outer_color.r() as f32, t)
+                                        as u8,
+                                    egui::lerp(center_color.g() as f32..=outer_color.g() as f32, t)
+                                        as u8,
+                                    egui::lerp(center_color.b() as f32..=outer_color.b() as f32, t)
+                                        as u8,
+                                )
+                            })
+                        } else {
+                            epaint::PathStroke::new(thickness, color)
+                        },
+                    ));
+                }
+
+                ui.painter().extend(shapes);
+            });
 
             ui.add(egui::github_link_file!(
                 "https://github.com/Cielquan/GamutPlotty/blob/main/",
