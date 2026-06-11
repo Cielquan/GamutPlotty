5d3f0c65a7069accd13d7d672cda4d854225623c
Christian Riedel <cielquan@protonmail.com>
2026-06-11 11:14:54 +0200

    change app to lib and clear logic

--- gamut_plotty/src/dummy_state.rs
+++
@@ -1,9 +0,0 @@
-use color_calc::CIELAB;
-
-pub fn create_color_points() -> Vec<CIELAB::LabPoint> {
-    vec![
-        CIELAB::LabPoint::new(0.0001, -0.0001, -0.0001).unwrap(),
-        CIELAB::LabPoint::new(50.0000, 0.0001, -0.0001).unwrap(),
-        CIELAB::LabPoint::new(100.0000, 0.0001, 0.0001).unwrap(),
-    ]
-}
--- gamut_plotty/src/lib.rs
+++ gamut_plotty/src/lib.rs
@@ -1,6 +1,3 @@
 #![warn(clippy::all, rust_2018_idioms)]
 
-mod app;
-mod dummy_state;
 mod gamut_data;
-pub use app::GamutPlottyApp;
